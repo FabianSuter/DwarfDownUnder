6666b060e9774b20fe46178849423ced0993a615
Fabian Suter <suter117@gmail.com>
2025-09-13 16:27:57 +0200

    Added gitignore

--- DwarfDownUnder/obj/Debug/net8.0/DwarfDownUnder.AssemblyInfo.cs
+++ DwarfDownUnder/obj/Debug/net8.0/DwarfDownUnder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DwarfDownUnder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75768401660379645c77cfcf369f6bd049c102c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc8ac92ce038e4710ecac734ff7906354f56fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DwarfDownUnder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DwarfDownUnder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
